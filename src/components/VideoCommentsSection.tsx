c97c32165fea99b3e9cc20b5560b01a31dd6439f
mgomez05 <gomezruss7@gmail.com>
2024-06-06 18:53:16 +0200

    Started writing the logic for fetching video comment data from the server

--- src/components/VideoCommentsSection.tsx
+++ src/components/VideoCommentsSection.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import VideoCommentList from './VideoCommentList';
 import { EXAMPLE_COMMENTS } from './VideoCard';
+import axios from 'axios';
 
 interface VideoCommentsSectionProps {
   video_id: string;
@@ -10,6 +11,30 @@ const VideoCommentsSection: React.FC<VideoCommentsSectionProps> = ({
   video_id,
 }) => {
   const [shouldShowComments, setShouldComments] = useState<boolean>(false);
+
+  const onShowCommentsButtonClicked = async () => {
+    // Fetch all comments for the video from the server
+    try {
+      const response = await axios.get(`/videos/comments?video_id=${video_id}`);
+
+      // If it's a success response, parse the data from the response
+      // Otherwise, log an error message
+      if (response.status === 200) {
+        setShouldComments(true);
+      } else {
+        console.error(
+          `ERROR: Error retrieving video comments from the server, server returned error code ${response.status}, and response ${response}`
+        );
+        setShouldComments(false);
+      }
+    } catch (error) {
+      console.error(
+        'ERROR: Error retrieving all region stats in MapboxComponent:',
+        error
+      );
+      setShouldComments(false);
+    }
+  };
 
   return (
     <div>
