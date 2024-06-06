87fa9f6de77b9692284ef39ede54caa752f6440f
mgomez05 <gomezruss7@gmail.com>
2024-06-06 13:59:30 +0200

    Updated the VideoCard to take a new video prop matching a new type called VideoPlaceholderFromAPI and updated the VideoCard to use the actual title, user id, and description passed to it

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
 
-interface VideoCardProps {}
+export type VideoPlaceholderFromAPI = {
+  user_id: string;
+  description: string;
+  image_thumbnail: string;
+  title: string;
+  video_id: string;
+};
 
-const VideoCard: React.FC<VideoCardProps> = ({}) => {
+interface VideoCardProps {
+  videoPlaceholder: VideoPlaceholderFromAPI;
+}
+
+const VideoCard: React.FC<VideoCardProps> = ({ videoPlaceholder }) => {
   return (
     <div className='border border-black rounded-lg px-3 bg-white'>
       <div className='flex flex-col items-center'>
-        <p>Title</p>
-        <p>Author</p>
+        <p>{videoPlaceholder.title}</p>
+        <p>{videoPlaceholder.user_id}</p>
       </div>
       <video controls>
         <source src='SampleVideo_1280x720_1mb.mp4' type='video/mp4' />
       </video>
-      <p>Description</p>
+      <p>{videoPlaceholder.description}</p>
     </div>
   );
 };
