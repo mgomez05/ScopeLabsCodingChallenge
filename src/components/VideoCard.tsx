fdd3b95f6e58a38487cbd6f2e4af5fcda3113e01
mgomez05 <gomezruss7@gmail.com>
2024-06-06 14:10:31 +0200

    Updated the VideoCard to take a new optional videoDataURI prop, if the videoDataURI is set, we show the video, otherwise we show the video thumbnail

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -19,18 +19,37 @@ export type VideoPlaceholderFromAPI = {
 
 interface VideoCardProps {
   videoPlaceholder: VideoPlaceholderFromAPI;
+  videoDataURI?: string;
 }
 
-const VideoCard: React.FC<VideoCardProps> = ({ videoPlaceholder }) => {
+const VideoCard: React.FC<VideoCardProps> = ({
+  videoPlaceholder,
+  videoDataURI,
+}) => {
+  // If we have a videoDataURI from the server, show the video
+  // Otherwise, show the video thumbnail
+  const videoCardContent = videoDataURI ? (
+    <video controls>
+      <source src={videoDataURI} type='video/mp4' />
+    </video>
+  ) : (
+    <img
+      src={videoPlaceholder.image_thumbnail}
+      alt={videoPlaceholder.title}
+    ></img>
+  );
+
   return (
     <div className='border border-black rounded-lg px-3 bg-white'>
       <div className='flex flex-col items-center'>
         <p>{videoPlaceholder.title}</p>
         <p>{videoPlaceholder.user_id}</p>
       </div>
-      <video controls>
-        <source src='SampleVideo_1280x720_1mb.mp4' type='video/mp4' />
-      </video>
+
+      {/* Show the video if we've retrieved the videoDataURI from the server
+          Otherwise, show the video thumbnail */}
+      {videoCardContent}
+
       <p>{videoPlaceholder.description}</p>
     </div>
   );
