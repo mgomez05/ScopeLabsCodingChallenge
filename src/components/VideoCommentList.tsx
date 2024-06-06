b344eb7d43e61cfd73c5e2117ce80af5f142d5bb
mgomez05 <gomezruss7@gmail.com>
2024-06-06 18:08:18 +0200

    Wrote a first draft of a VideoCommentList components

---
+++ src/components/VideoCommentList.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import VideoComment from './VideoComment';
+
+const EXAMPLE_COMMENTS = [
+  {
+    commentAuthor: 'Johnny Appleseed',
+    commentText: 'This is a great video!',
+  },
+  {
+    commentAuthor: 'Jane Doe',
+    commentText: 'I love this video!',
+  },
+  {
+    commentAuthor: 'John Smith',
+    commentText: 'This video is amazing!',
+  },
+];
+
+const VideoCommentList: React.FC = () => {
+  return (
+    <div className='border border-black rounded-lg bg-white'>
+      <p className='text-center text-3xl font-bold pt-2 px-2'>Comments</p>
+      <div className='flex flex-col gap-y-3 pb-2'>
+        {EXAMPLE_COMMENTS.map((comment, index) => (
+          <VideoComment
+            key={index}
+            commentAuthor={comment.commentAuthor}
+            commentText={comment.commentText}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default VideoCommentList;
