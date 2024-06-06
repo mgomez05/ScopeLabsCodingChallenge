e379324804d6269acef88e762c3356545cd6762c
mgomez05 <gomezruss7@gmail.com>
2024-06-06 13:48:17 +0200

    Added a title, author, and description placeholder to the VideoCard

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -4,9 +4,16 @@ interface VideoCardProps {}
 
 const VideoCard: React.FC<VideoCardProps> = ({}) => {
   return (
-    <video controls>
-      <source src='SampleVideo_1280x720_1mb.mp4' type='video/mp4' />
-    </video>
+    <div>
+      <div className='flex flex-col items-center'>
+        <p>Title</p>
+        <p>Author</p>
+      </div>
+      <video controls>
+        <source src='SampleVideo_1280x720_1mb.mp4' type='video/mp4' />
+      </video>
+      <p>Description</p>
+    </div>
   );
 };
 
