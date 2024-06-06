adaead496c7186790df4a36515ddde662e2f0350
mgomez05 <gomezruss7@gmail.com>
2024-06-06 18:03:44 +0200

    Removed the black border from the VideoComment component and replaced it with a flex flex-col tailwind styling

--- src/components/VideoComment.tsx
+++ src/components/VideoComment.tsx
@@ -10,7 +10,7 @@ const VideoComment: React.FC<VideoCommentProps> = ({
   commentText,
 }) => {
   return (
-    <div className='border border-black rounded-lg px-3 bg-white'>
+    <div className='flex flex-col rounded-lg px-3 bg-white'>
       <p className='font-bold'>{commentAuthor}</p>
       <p>{commentText}</p>
     </div>
