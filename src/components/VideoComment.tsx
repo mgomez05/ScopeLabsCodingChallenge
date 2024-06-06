cd1929275299e8462b055389faa4ea3439f75a08
mgomez05 <gomezruss7@gmail.com>
2024-06-06 17:44:38 +0200

    Added a VideoComment component to the project

---
+++ src/components/VideoComment.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const VideoComment: React.FC = () => {
+  return (
+    <div className='border border-black rounded-lg px-3 bg-white'>
+      <p>author</p>
+      <p>comment</p>
+    </div>
+  );
+};
+
+export default VideoComment;
