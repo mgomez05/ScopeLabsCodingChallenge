e9c6268341a816d65d6be0de46d63dc4bbe952d6
mgomez05 <gomezruss7@gmail.com>
2024-06-07 15:03:06 +0200

    Updated the learnwellHeader component to use the new button component

--- src/components/LearnwellHeader.tsx
+++ src/components/LearnwellHeader.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Button from './Button';
 
 const LearnwellHeader: React.FC = () => {
   return (
     <div className='flex flex-row items-center justify-between'>
       <p>Check out our videos!</p>
       <img width='155px' src='full_logo_color.png' alt='Learnwell Logo'></img>
-      <button>Click me</button>
+      <Button onClick={() => console.log('Button clicked!')}>Upload</Button>
     </div>
   );
 };
