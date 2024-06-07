5abe918fecfad4e4dfc6df0522f5a2558c710785
mgomez05 <gomezruss7@gmail.com>
2024-06-07 19:42:20 +0200

    Updated the Home page to be divided using a grid, with the VideoGallery taking up 2/3 the height and the create video form taking up 1/3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import LearnwellHeader from '@/components/LearnwellHeader';
 import { useRouter } from 'next/router';
 
 import useFetchVideos from '@/components/hooks/useFetchVideos';
+import CreateVideoButton from '@/components/CreateVideoButton';
 
 export default function Home() {
   const router = useRouter();
@@ -11,18 +12,27 @@ export default function Home() {
   const { allVideos } = useFetchVideos();
 
   return (
-    <main className={`min-h-screen px-24`}>
+    <div className='grid grid-rows-3 min-h-screen'>
       <LearnwellHeader />
-      <VideoGallery
-        onVideoClick={(videoId) => {
-          router.push({
-            pathname: '/video',
-            query: { video_id: videoId },
-          });
-        }}
-        videoList={allVideos}
-        className='grid grid-cols-3 gap-4 overflow-y-auto'
-      />
-    </main>
+
+      {/* Video Gallery */}
+      <div className='row-span-2 p-4'>
+        <VideoGallery
+          onVideoClick={(videoId) => {
+            router.push({
+              pathname: '/video',
+              query: { video_id: videoId },
+            });
+          }}
+          videoList={allVideos}
+          className='grid grid-cols-3 gap-4 overflow-y-auto'
+        />
+      </div>
+
+      {/* Create Video Form */}
+      <div className='row-span-1 p-4'>
+        <CreateVideoButton />
+      </div>
+    </div>
   );
 }
