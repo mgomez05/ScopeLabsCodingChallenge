6ff6c2e9665b6b6c64bb8cf0a6bc42e5cb9ebc7e
mgomez05 <gomezruss7@gmail.com>
2024-06-07 19:37:47 +0200

    Updated the VideoGallery component to show an error message if no videos can be retrieved from the server

--- src/components/VideoGallery.tsx
+++ src/components/VideoGallery.tsx
@@ -14,20 +14,31 @@ const VideoGallery: React.FC<VideoGalleryProps> = ({
   className,
 }) => {
   return (
-    <div className={className}>
-      {/* Render the list of videos */}
-      {videoList.map((video, index) => (
-        <div
-          onClick={() => {
-            onVideoClick(video.videoMetaData.video_id);
-          }}
-        >
-          <VideoCard key={index} videoMetaData={video.videoMetaData} />
-        </div>
-      ))}
+    <>
+      {/* If there are videos to display, render them
+          Otherwise show an error message */}
+      {videoList.length > 0 ? (
+        <div className={className}>
+          {/* Render the list of videos */}
+          {videoList.map((video, index) => (
+            <div
+              onClick={() => {
+                onVideoClick(video.videoMetaData.video_id);
+              }}
+            >
+              <VideoCard key={index} videoMetaData={video.videoMetaData} />
+            </div>
+          ))}
 
-      <CreateVideoButton />
-    </div>
+          <CreateVideoButton />
+        </div>
+      ) : (
+        // If there are no videos to display, show an error message to the user
+        <p className='text-lg font-semibold text-center'>
+          No videos could be retrieved from the server, please try again later
+        </p>
+      )}
+    </>
   );
 };
 
