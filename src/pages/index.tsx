a625d00d87f63027d15be90e7bd2c2bc11698f5d
mgomez05 <gomezruss7@gmail.com>
2024-06-07 17:26:46 +0200

    Updated all the text in the web page to be the Outfit font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,20 @@ import VideoGallery from '@/components/VideoGallery';
 import React from 'react';
 import LearnwellHeader from '@/components/LearnwellHeader';
 import { SAMPLE_VIDEO_LIST } from '@/sampleData/sampleData';
+import { useRouter } from 'next/router';
 
 export default function Home() {
+  const router = useRouter();
+
   return (
     <main className={`min-h-screen px-24`}>
       <LearnwellHeader />
       <VideoGallery
         onVideoClick={(videoId) => {
-          console.log(`Clicked on video with ID: ${videoId}`);
+          router.push({
+            pathname: '/video',
+            query: { video_id: videoId },
+          });
         }}
         videoList={SAMPLE_VIDEO_LIST}
         className='grid grid-cols-3 gap-4 overflow-y-auto'
