ff711d68695980742ada67c5cf92e1b68907bdcd
mgomez05 <gomezruss7@gmail.com>
2024-06-07 16:25:56 +0200

    Added vertical padding to the LearnwellHeader

--- src/components/LearnwellHeader.tsx
+++ src/components/LearnwellHeader.tsx
@@ -3,7 +3,7 @@ import Button from './Button';
 
 const LearnwellHeader: React.FC = () => {
   return (
-    <div className='flex flex-row items-center justify-between'>
+    <div className='flex flex-row items-center justify-between py-4'>
       <p>Check out our videos!</p>
       <img width='155px' src='full_logo_color.png' alt='Learnwell Logo'></img>
       <Button onClick={() => console.log('Button clicked!')}>Upload</Button>
