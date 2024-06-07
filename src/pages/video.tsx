cc92b0099f3f84c623b274586dfa2befd8890c48
mgomez05 <gomezruss7@gmail.com>
2024-06-07 16:18:55 +0200

    Wrote a draft of the Single Video Page component, called VideoPage

---
+++ src/pages/video.tsx
@@ -0,0 +1,31 @@
+import LearnwellHeader from '@/components/LearnwellHeader';
+import VideoCard from '@/components/VideoCard';
+import VideoGallery from '@/components/VideoGallery';
+import { SAMPLE_VIDEO_LIST } from '@/sampleData/sampleData';
+import React from 'react';
+
+// A page for showing a single video,  with a vertical list of videos
+// on the right hand side
+export default function VideoPage() {
+  return (
+    <div className='flex flex-col'>
+      <LearnwellHeader />
+      <div className='grid grid-cols-3 gap-4'>
+        <div className='col-span-2 p-4'>
+          <VideoCard
+            videoDataURI={SAMPLE_VIDEO_LIST[0].videoDataURI}
+            videoMetaData={SAMPLE_VIDEO_LIST[0].videoMetaData}
+          />
+        </div>
+        <div className='p-4'>
+          <VideoGallery
+            videoList={SAMPLE_VIDEO_LIST}
+            onVideoClick={() => {
+              console.log('Someone clicked a video on the side list');
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
