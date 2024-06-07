25b5c2c04c9bb164596499eec58e054823fea78f
mgomez05 <gomezruss7@gmail.com>
2024-06-07 14:45:47 +0200

    Updated the LearnwellHeader's logo to be 155px width to match the figma

--- src/components/LearnwellHeader.tsx
+++ src/components/LearnwellHeader.tsx
@@ -4,7 +4,7 @@ const LearnwellHeader: React.FC = () => {
   return (
     <div className='flex flex-row items-center justify-between'>
       <p>Check out our videos!</p>
-      <img src='full_logo_color.png' alt='Learnwell Logo'></img>
+      <img width='155px' src='full_logo_color.png' alt='Learnwell Logo'></img>
       <button>Click me</button>
     </div>
   );
