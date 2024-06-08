2ab91b1132777f34c1dbf56f75e7d5d90ec5d4b0
mgomez05 <gomezruss7@gmail.com>
2024-06-08 02:57:20 +0200

    Added logic to the AddCommentSection.tsx for sending a POST videos/comments request when the user clicks on the Comment button

--- src/components/AddCommentSection.tsx
+++ src/components/AddCommentSection.tsx
@@ -1,5 +1,9 @@
 import React, { ChangeEvent, useState } from 'react';
 import Button from './Button';
+import axios from 'axios';
+import { ServerVideoComment } from './VideoCommentsSection';
+
+const SOME_OTHER_USER_ID = 'john_smitherson';
 
 type AddCommentSectionProps = {
   video_id: string;
@@ -7,6 +11,46 @@ type AddCommentSectionProps = {
 
 const AddCommentSection: React.FC<AddCommentSectionProps> = ({ video_id }) => {
   const [newCommentText, setNewCommentText] = useState<string>('');
+
+  // Attempts to send the comment to the server
+  // - If the server gives us a 200 or 201 status code, we show a success message
+  // - Otherwise, we show an error message
+  const onCommentButtonClicked = async () => {
+    try {
+      const newServerVideoComment: ServerVideoComment = {
+        user_id: SOME_OTHER_USER_ID,
+        content: newCommentText,
+        video_id: video_id,
+      };
+
+      const response = await axios.post(
+        `/videos/comments`,
+        newServerVideoComment
+      );
+
+      // If it's a success response, display a success message and reset the comment textbox
+      // Otherwise, log an error message and display an error message
+      if (response.status == 200 || response.status === 201) {
+        // Show a success message
+        //setSuccessMessage('Video has been uploaded successfully!');
+
+        // Reset the comment textbox
+        setNewCommentText('');
+      } else {
+        // setErrorMessage(ERROR_UPLOADING_VIDEO_TO_SERVER_MESSAGE);
+        console.error(
+          `ERROR: Error adding a comment for the video with id '${video_id}', status code was`,
+          response.status
+        );
+      }
+    } catch (error) {
+      //setErrorMessage(ERROR_UPLOADING_VIDEO_TO_SERVER_MESSAGE);
+      console.error(
+        `ERROR: Error adding a comment for the video with id '${video_id}', here's the error:`,
+        error
+      );
+    }
+  };
 
   return (
     <div className='flex flex-row justify-end px-2'>
@@ -20,7 +64,7 @@ const AddCommentSection: React.FC<AddCommentSectionProps> = ({ video_id }) => {
         }
       />
       <div className=''>
-        <Button onClick={() => console.log()}>Comment</Button>
+        <Button onClick={() => onCommentButtonClicked()}>Comment</Button>
       </div>
     </div>
   );
